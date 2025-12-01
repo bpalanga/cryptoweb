f0b6e22f25ec99701021ff379859d94470ef9d7d
bpalanga <algustopalanga@gmail.com>
2025-12-01 13:16:29 +0300

    Initial commit of the cryptoweb project

---
+++ cryptoweb-main/config.py
@@ -0,0 +1,8 @@
+# config.py
+db_config = {
+    'host': 'localhost',
+    'user': 'root',  # XAMPP default username
+    'password': '',  # XAMPP default is empty password
+    'database': 'credit_vault_db',  # Changed to new database name
+    'charset': 'utf8mb4'
+}
